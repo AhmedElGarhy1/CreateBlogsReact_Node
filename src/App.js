8116445a6275a336f216c4fe27ea711f7e0b3b54
Ahmed ElGarhy <89610486+AhmedElGarhy1@users.noreply.github.com>
2022-06-20 22:51:56 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
